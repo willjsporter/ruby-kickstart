6589485b6d8970cae6235d8e8d0fe631930ef73f
Will Porter <willjsporter@hotmail.co.uk>
2017-07-13 03:12:17 +0100

    rest of session1

--- session1/challenge/4_logic.rb
+++ session1/challenge/4_logic.rb
@@ -1,6 +1,6 @@
 # A grad student at a local university thinks he has discovered a formula to
-# predict what kind of grades a person will get. He says if you own less than 
-# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C", 
+# predict what kind of grades a person will get. He says if you own less than
+# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C",
 # and if you own more than 20 books, you will get a "B".
 # He further hypothesizes that if you actually read your books, then you will
 # get a full letter grade higher in every case.
@@ -10,4 +10,9 @@
 # grade(15, true)   # => "B"
 
 def grade(num_books, reads_books)
+  if reads_books
+    num_books>20 ? 'A' : num_books<10 ? 'C' : 'B'
+  else
+    num_books>20 ? 'B' : num_books<10 ? 'D' : 'C'
+  end
 end
--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,24 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+    i=0; retval =""
+    string.length.times do |i|
+      if i.even? && !return_odds
+        retval = retval+string[i]
+      elsif !i.even? && return_odds
+        retval = retval+string[i]
+      end
+    end
+    retval
 end
+
+#want to return every other element (changes if even or odd)
+#so will do arrstr.length.to_f/2 if 50 spaces then do 0 to 24 so 25 good
+#if 51 spaces then do 0 to 25 so 26
